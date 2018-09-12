7d4d5ac135d5c07044bdb81ae6b44251364c4c52
cice <cice@A8POVMac11.local>
2018-09-12 21:24:58 +0200

    Initial commit

---
+++ src/main/java/com/cice/maven/test/java/ServletTest.java
@@ -0,0 +1,22 @@
+package com.cice.maven.test.java;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name="ServletTest", urlPatterns = "/test")
+public class ServletTest extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("Holi Mundo from Servlet!!");
+    }
+
+}
